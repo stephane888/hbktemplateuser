103ec234315f88ac02eb9a7e8bfc47c33090ac27
kouwa stephane <kksasteph888@gmail.com>
2022-08-29 15:05:49 +0200

    MAJ

--- src/Controller/HbktemplateuserController.php
+++ src/Controller/HbktemplateuserController.php
@@ -3,6 +3,7 @@
 namespace Drupal\hbktemplateuser\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 
 /**
  * Returns responses for hbktemplateuser routes.
@@ -13,12 +14,23 @@ class HbktemplateuserController extends ControllerBase {
    * Builds the response.
    */
   public function build() {
-
+    $links = [];
+    $links[] = [
+      '#type' => 'link',
+      '#title' => 'exportation du theme',
+      '#url' => Url::fromRoute('export_import_entities.generatesite'),
+      '#attributes' => [
+        'class' => [
+          'toolbar-icon'
+        ]
+      ]
+    ];
+    // $this->ExportEntities->getEntites();
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('It works! ..')
     ];
-
+    $build['links'] = $links;
     return $build;
   }
 
