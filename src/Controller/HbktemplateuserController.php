3cb33a4c58ac677dc1136d9a90490f04408f90d0
kouwa stephane <kksasteph888@gmail.com>
2022-09-07 21:32:18 +0200

    Maj

--- src/Controller/HbktemplateuserController.php
+++ src/Controller/HbktemplateuserController.php
@@ -4,6 +4,9 @@ namespace Drupal\hbktemplateuser\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\domain\DomainNegotiator;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for hbktemplateuser routes.
@@ -11,13 +14,34 @@ use Drupal\Core\Url;
 class HbktemplateuserController extends ControllerBase {
 
   /**
+   *
+   * @var DomainNegotiator
+   */
+  protected $DomainNegotiator;
+
+  /**
+   * --
+   */
+  function __construct(DomainNegotiator $DomainNegotiator) {
+    $this->DomainNegotiator = $DomainNegotiator;
+  }
+
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('domain.negotiator'));
+  }
+
+  /**
    * Builds the response.
    */
-  public function build() {
+  public function build(Request $Request) {
     $links = [];
     $links[] = [
       '#type' => 'link',
-      '#title' => 'exportation du theme',
+      '#title' => ' Exporter le theme encours ',
       '#url' => Url::fromRoute('export_import_entities.generatesite'),
       '#attributes' => [
         'class' => [
@@ -25,12 +49,67 @@ class HbktemplateuserController extends ControllerBase {
         ]
       ]
     ];
+
+    if (\Drupal::moduleHandler()->moduleExists('domain') && $domain = $this->DomainNegotiator->getActiveDomain()) {
+
+      $themeConf = $this->entityTypeManager()->getStorage("config_theme_entity")->loadByProperties([
+        'hostname' => $domain->id()
+      ]);
+      if ($themeConf) {
+        $themeConf = reset($themeConf);
+        // entity.config_theme_entity.edit_form
+        $links[] = [
+          '#type' => 'link',
+          '#title' => ' Configurer les couleurs, tailles de polices, logo ',
+          '#url' => Url::fromRoute('entity.config_theme_entity.edit_form', [
+            'config_theme_entity' => $themeConf->id()
+          ], [
+            'query' => [
+              'destination' => $Request->getPathInfo()
+            ]
+          ]),
+          '#attributes' => [
+            'class' => [
+              'toolbar-icon'
+            ]
+          ]
+        ];
+      }
+    }
+
+    $links[] = [
+      '#type' => 'link',
+      '#title' => ' Definir des styles personnalisés pour le theme ',
+      '#url' => Url::fromRoute('generate_style_theme.managecustom.styles'),
+      '#attributes' => [
+        'class' => [
+          'toolbar-icon'
+        ]
+      ]
+    ];
+    $links[] = [
+      '#type' => 'link',
+      '#title' => ' Voir la liste des fichiers de styles ',
+      '#url' => Url::fromRoute('layoutgenentitystyles.generate'),
+      '#attributes' => [
+        'class' => [
+          'toolbar-icon'
+        ]
+      ]
+    ];
+    /* */
+    //
     // $this->ExportEntities->getEntites();
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works! ..')
+      '#type' => 'html_tag',
+
+      '#tag' => 'h2',
+      '#value' => $this->t("Liste d'actions")
     ];
-    $build['links'] = $links;
+    $build['links'] = [
+      '#theme' => 'item_list',
+      '#items' => $links
+    ];
     return $build;
   }
 
